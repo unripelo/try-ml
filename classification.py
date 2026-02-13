4b06ed4745bfaa43aba260e53be836608c48129f
Ira Narisma <narisma.ira.chloie@gmail.com>
2026-02-13 23:22:28 +0800

    add lib

--- classification.py
+++ classification.py
@@ -1,3 +1,6 @@
-from sklear.datasets import load_breast_cancer
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression     
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
