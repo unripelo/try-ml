876a17efa18812da8ced3d89116ae10368ac56e9
Ira Narisma <narisma.ira.chloie@gmail.com>
2026-02-15 22:43:39 +0800

    load, train/split, scaler data

--- classification.py
+++ classification.py
@@ -4,3 +4,14 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression     
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+data = load_breast_cancer()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
