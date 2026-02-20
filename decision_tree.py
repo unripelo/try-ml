701d57a6358a0aec17b72894c98ce8393caa8000
Ira Narisma <narisma.ira.chloie@gmail.com>
2026-02-20 22:18:12 +0800

    add new file

---
+++ decision_tree.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
