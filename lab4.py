bc36c53de200209baa6651a828fd43c341d69031
Ira Narisma <narisma.ira.chloie@gmail.com>
2026-02-23 22:46:30 +0800

    function

--- lab4.py
+++ lab4.py
@@ -1,3 +1,20 @@
 import sys
 import threading
 import time
+
+SPINNER_FRAMES = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
+SPINNER_INTERVAL = 0.08
+
+
+def _spinner_worker(stop_event: threading.Event, message: str) -> None:
+    """Run spinner animation until stop_event is set."""
+    idx = 0
+    while not stop_event.is_set():
+        frame = SPINNER_FRAMES[idx % len(SPINNER_FRAMES)]
+        sys.stdout.write(f"\r  {frame}  {message}")
+        sys.stdout.flush()
+        idx += 1
+        stop_event.wait(SPINNER_INTERVAL)
+    sys.stdout.write("\r" + " " * (len(message) + 8) + "\r")
+    sys.stdout.flush()
+
